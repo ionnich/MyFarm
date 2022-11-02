d4c077916985012feee254833e8694deca952b8d
Aaron Gumapac <aarn.gmpc@gmail.com>
2022-11-02 15:28:15 +0800

    fix: 🐛 finalized Seed class

---
+++ src/main/Seed.java
@@ -0,0 +1,12 @@
+package main;
+
+public class Seed {
+
+    String seedName;
+    int cost;
+
+    public Seed(String seedName, int cost) {
+        this.seedName = seedName;
+        this.cost = cost;
+    }
+}
